6d5a633779041b7b9332e29a9efd0922557ae2eb
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2025-02-11 20:49:18 +0530

    Challenges Quiz practie question

---
+++ week_03/JavaScript/ChallengesQuiz/function/challenges_03.js
@@ -0,0 +1,11 @@
+// You're packing chocolate bars into gift boxes. Each box mus contain the same number of chocolate bars. How many chocolate bars do you need for all the boxes./
+
+//---------------Challenge-------------------------
+// create function that multiplies two numbers (chocolate bars per box and number of boxes ) and returns the total numbers of chocolate bars needed.
+
+function totalChocolateBars(barsPerBox, numberOfBoxes){
+    return barsPerBox * numberOfBoxes
+}
+
+let ChocolateTotal = totalChocolateBars(43,65)
+console.log(ChocolateTotal)
---
+++ week_03/JavaScript/classCode/script_02.js
@@ -0,0 +1,18 @@
+function printChai(){
+
+    console.log("Hello Chai")
+}
+
+function bringBrush(numberOfBrush){
+    console.log(`Hanji, le aaeye ${numberOfBrush} brush`)
+}
+
+function addTwo(num1, num2){
+    return num1 + num2
+}
+
+let final  = addTwo(43,5)
+console.log(final)
+console.log(addTwo(32,5))
+printChai()
+bringBrush(4)
