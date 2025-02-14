c86fe26c81f6f820cee9b6e7280fef028550e040
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2025-02-14 16:06:10 +0530

    memory and objects concept is clear

---
+++ week_03/JavaScript/classCode/ObjectPiyushSir/object.js
@@ -0,0 +1,39 @@
+const person = {
+    x: 10,
+    firstName: 'Jitu',
+    lastName: 'raj sharma',
+    hobbies: ['Coding', 'Gym'],
+    isMarried: false,
+    hasGf: false,
+    getFullNamea: function(){
+        return 'Jiturajsharma'
+    },
+    address: {
+        hno: 1,
+        street: 1,
+        countryCode: 'IN',
+        state: 'PB'
+    }
+}
+
+console.log(person.address.countryCode)
+
+console.log(person.hobbies)
+
+console.log(person.firstName)
+console.log(person.lastName)
+console.log(person)
+console.log(person.x)
+
+
+const remote = {
+    color: 'black',
+    brand: "xyz",
+    dimensions: { height: 1, width: 1},
+    turnOff: function(){
+
+    },
+    volumeUp: function(){
+
+    }
+}
