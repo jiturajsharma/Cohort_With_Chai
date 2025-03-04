c9edc1e83e89d0aeed28f05e0dc91007bc51e115
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2025-03-04 22:42:43 +0530

    prototype chain

---
+++ Self_Prep_Interview_Questions/Question_02.js
@@ -0,0 +1,6 @@
+/**
+ * What is a prototype chain
+ * Prototype chaining:-> prototype chaining is used to build new types of objects based on existing ones. It is similar to inheritance in a class based language.
+ * 
+ * The prototype on object instance is available through Object.getPrototypeOf(object) or proto property wherears prototypes on constructors function is available through Object.prototype.
+ */
