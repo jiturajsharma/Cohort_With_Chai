6d20427d6a8d878603e06f1021baa0b53f304578
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2025-04-12 17:06:24 +0530

    typescript added module

--- type_script/src/index.ts
+++ type_script/src/index.ts
@@ -1,11 +1,19 @@
 import http from 'http';
+import { env } from './env';
+import { logger } from './logger';
+import { createApp } from './app';
 
-async function main(){
+async function main() {
     try {
-        const PORT: number = +(process.env.PORT ?? 3000);
-        const server = http.createServer();
-        server.listen(PORT);
-    } catch (error) {
-        
+        const PORT: number = +(env.PORT ?? 8000);
+
+    const server = http.createServer(createApp());
+    server.listen(PORT, () => {
+        logger.info(`Server is running on PORT ${PORT}`);
+    });
+    } catch (err) {
+    logger.error(`Error starting server`, err);
     }
 }
+
+main();
