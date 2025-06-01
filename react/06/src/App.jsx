11d559d0ebb7e65d10afee3cf35648ca424f5e57
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2025-06-01 15:28:02 +0530

    custum hooks use

--- react/06/src/App.jsx
+++ react/06/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react"
+import { useSpecialChai } from "./hooks/useSpecialChai"
 
 export function App(){
+    const {chai, loading, error} = useSpecialChai()
     const [message, setMessage] = useState("Loading...")
 
     useEffect( () =>{  // components mounts
@@ -14,11 +16,16 @@ export function App(){
 // cleanup function is also called
         // return ()    // components unmount 
     }, []  ) // components did updatation
+
+if(loading) return <h2>Loading...</h2>
+if(error) return <h2>Error:</h2>
+
     return(
         <div>
             <h1>Welcome to chai code</h1>
             <p>Serving hot chai with react</p>
             <h2>{message}</h2>
+            <h3>{chai.name}</h3>
         </div>
     )
 }
