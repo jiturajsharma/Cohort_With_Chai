d5f868753c4dadfc8c37cc3b5fc2fcd252f68434
Jitu Vishwakarma <jiturajsharma8093@gmail.com>
2025-09-03 16:03:48 +0530

    Add response.ok check in API fetch function

---
+++ reactoverview/frontend/service/apiClient.js
@@ -0,0 +1,60 @@
+class ApiClient{
+    constructor(){
+        this.baseURL = "http://localhost:3000/api/v1";
+        this.defaultHeaders = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    }
+
+    async customFetch(endpoint, options = {} ) {
+        try {
+            const url = `${this.baseURL} ${endpoint}`;
+            const headers = {...this.defaultHeaders, ...options.headers}
+
+            const config = {
+                ...options,
+                headers, 
+                credentials: 'include'
+            }
+
+            console.log(`Fetching ${url}`);
+            await fetch(url, config)
+            // check if respnse.ok === value todo make it
+
+            const response = await fetch(url, config)
+            const data = await response.json()
+            return data
+        } catch (error) {
+            console.error('API Error', error)
+            throw error
+        }
+    }
+
+    // auth endpoints
+
+    async signup(name, email, password){
+        return this.customFetch("/users/register", {
+            method: "POST",
+            body: JSON.stringify({name, email, password})
+        })
+    }
+
+        async login(email, password){
+        return this.customFetch("/users/login", {
+            method: "POST",
+            body: JSON.stringify({ email, password})
+        })
+    }
+
+        async getProfile(){
+        this.customFetch("/users/me", {
+            method: "POST",
+        })
+    }
+}
+
+const apiClient = new ApiClient();
+
+export default apiClient;
+
